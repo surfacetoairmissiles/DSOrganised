b89e642fcb12e04cbd71e2f3e7ddf9831c6c191d
surfacetoairmissiles <surfacetoairmissiles@gmail.com>
2017-01-24 23:23:24 +1300

    Final commit
    before i remerge and start working on interrupts

---
+++ game/draw.lua
@@ -0,0 +1,67 @@
+function rendericons()
+	--Draw the icons
+	love.graphics.setColor(255, 255, 255, 255)
+
+	love.graphics.draw(icons.calendar,35,40)
+	love.graphics.draw(icons.planner,125,40)
+	love.graphics.draw(icons.letter,215,40)
+	love.graphics.draw(icons.todo,35,130)
+	love.graphics.draw(icons.scribble,125,130)
+	
+
+	--icon labels
+	love.graphics.setColor(0,0,0,255)
+	love.graphics.setFont(fonts.small)
+
+	love.graphics.print("Calendar", 47, 95)
+	love.graphics.printf("Day Planner", 125, 95, 70, "center")
+	love.graphics.printf("Address", 215, 95, 70, "center")
+	love.graphics.printf("Todo List", 35, 185, 70, "center")
+	love.graphics.printf("Scribble Pad", 125, 185, 70, "center")
+	love.graphics.printf("Browse", 215, 185, 70, "center")
+	love.graphics.setFont(fonts.large)
+	love.graphics.printf("Configuration", 70, 213,110, "center")
+end
+
+function rendertopscreen()
+	drawtoTopScreen()
+	--Start Drawing the boxes--
+	love.graphics.setColor(190,190,190,255)
+	drawrectangle("fill", 0, 0, 400, 40)-- 1st grey box
+	love.graphics.setColor(0,0,0,255)
+	drawrectangle("fill",0, 40, 400, 5) --1st black bar
+	drawrectangle("line", 5, 60, 301, 20) --Reminders box
+	drawrectangle("line", 316, 60, 75, 150) -- Placeholder box
+	drawrectangle("line", 5, 95, 301, 50) -- Day planner
+	drawrectangle("line", 5, 160, 301, 50) --Todo
+	drawrectangle("fill",0, 215, 400, 5) --2nd black bar
+	love.graphics.setColor(190,190,190,255)
+	drawrectangle("fill", 0, 220, 400, 20)-- 2nd grey box
+
+	--Draw the text--
+
+	--Top time and datre Text--
+	love.graphics.setColor(0,0,0,255)
+	love.graphics.setFont(fonts.large)
+	love.graphics.print(os.date("%A, %B %d, %Y"), 5, 2)
+	love.graphics.print(os.date("%I:%M:%S %p"), 5, 20)
+	love.graphics.print(currentos, 200, 20)
+
+	--titles
+	love.graphics.setFont(fonts.small)
+	love.graphics.print("Reminder", 5, 50)
+	love.graphics.print("Placeholder", 316, 50)
+	love.graphics.print("Day Planner", 5, 85)
+	love.graphics.print("Todo List", 5, 150)
+
+	-- Welcome message
+	love.graphics.setFont(fonts.large)
+	love.graphics.print("Welcome, "..user.."!", 5, 222)
+end
+
+function drawwindowsoutline()
+if currentos == "Windows" then -- This sets an outline so i can see the bounds of the screen
+		love.graphics.setColor(0,0,0,255)
+		love.graphics.rectangle("line", 0, 0, 320, 240)
+	end
+end
--- game/main.lua
+++ game/main.lua
@@ -1,4 +1,4 @@
-
+require "draw"
 
 user = "Sam"
 currentos = love.system.getOS()
@@ -36,48 +36,13 @@ end
 
 function love.draw()
 	--Draw stuff on the top screen --
-	drawtoTopScreen()
-	--Start Drawing the boxes--
-	love.graphics.setColor(190,190,190,255)
-	drawrectangle("fill", 0, 0, 400, 40)-- 1st grey box
-	love.graphics.setColor(0,0,0,255)
-	drawrectangle("fill",0, 40, 400, 5) --1st black bar
-	drawrectangle("line", 5, 60, 301, 20) --Reminders box
-	drawrectangle("line", 316, 60, 75, 150) -- Placeholder box
-	drawrectangle("line", 5, 95, 301, 50) -- Day planner
-	drawrectangle("line", 5, 160, 301, 50) --Todo
-	drawrectangle("fill",0, 215, 400, 5) --2nd black bar
-	love.graphics.setColor(190,190,190,255)
-	drawrectangle("fill", 0, 220, 400, 20)-- 2nd grey box
-
-	--Draw the text--
-
-	--Top time and datre Text--
-	love.graphics.setColor(0,0,0,255)
-	love.graphics.setFont(fonts.large)
-	love.graphics.print(os.date("%A, %B %d, %Y"), 5, 2)
-	love.graphics.print(os.date("%I:%M:%S %p"), 5, 20)
-	love.graphics.print(currentos, 200, 20)
-
-	--titles
-	love.graphics.setFont(fonts.small)
-	love.graphics.print("Reminder", 5, 50)
-	love.graphics.print("Placeholder", 316, 50)
-	love.graphics.print("Day Planner", 5, 85)
-	love.graphics.print("Todo List", 5, 150)
-
-	-- Welcome message
-	love.graphics.setFont(fonts.large)
-	love.graphics.print("Welcome, "..user.."!", 5, 222)
+	rendertopscreen()
 
 
 	--Draw stuff on the bottom screen
 	drawtoBottomScreen()
-	
-	if currentos == "Windows" then -- This sets an outline so i can see the bounds of the screen
-		love.graphics.setColor(0,0,0,255)
-		love.graphics.rectangle("line", 0, 0, 320, 240)
-	end
+	--Draw a window outline so that a windows user can see the outline of the window
+	drawwindowsoutline()
 	--Green boxes to indicate icon and text position *placeholder*
 	love.graphics.setColor(45, 69, 34, 255) -- Get some green
 
@@ -89,28 +54,7 @@ function love.draw()
 	drawrectangle("line", 215, 130, 70, 70)
 	drawrectangle("line", 70, 213, 180, 20)
 
-	--Draw the icons
-	love.graphics.setColor(255, 255, 255, 255)
-
-	love.graphics.draw(icons.calendar,35,40)
-	love.graphics.draw(icons.planner,125,40)
-	love.graphics.draw(icons.letter,215,40)
-	love.graphics.draw(icons.todo,35,130)
-	love.graphics.draw(icons.scribble,125,130)
-	
-
-	--Text of app icons
-	love.graphics.setColor(0,0,0,255)
-	love.graphics.setFont(fonts.small)
-
-	love.graphics.print("Calendar", 47, 95)
-	love.graphics.printf("Day Planner", 125, 95, 70, "center")
-	love.graphics.printf("Address", 215, 95, 70, "center")
-	love.graphics.printf("Todo List", 35, 185, 70, "center")
-	love.graphics.printf("Scribble Pad", 125, 185, 70, "center")
-	love.graphics.printf("Browse", 215, 185, 70, "center")
-	love.graphics.setFont(fonts.large)
-	love.graphics.printf("Configuration", 70, 213,110, "center")
+	rendericons()
 
 
 end
